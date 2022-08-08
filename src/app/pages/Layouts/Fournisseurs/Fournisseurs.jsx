2411b23e395164aa2afcef8997697a5f6a40d3cb
ouiam <ouiam.abi29@gmail.com>
2022-08-08 18:15:18 +0100

    add client panel

--- src/app/pages/Layouts/Fournisseurs/Fournisseurs.jsx
+++ src/app/pages/Layouts/Fournisseurs/Fournisseurs.jsx
@@ -1,10 +1,51 @@
 import React from "react";
-
+import "./fournisseurs.scss";
 
 const Fournisseurs=()=>{
     return(
         <>
-        <h1>hello from Fournisseurs</h1>
+        <section id="Client">
+        <nav class="navbar bg-light">
+  <form class="container-fluid justify-content-start">
+    <button class="b1 ">Plus</button>
+    <button class="b2">Plus</button>
+    <button class="b3">Plus</button>
+  </form>
+</nav>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">SOCIETE</th>
+      <th scope="col">ADRESSE</th>
+      <th scope="col">ICE</th>
+      <th scope="col">TELEPHONE</th>
+      <th scope="col">EMAIL</th>
+      <th scope="col">BANQUE</th>
+      <th scope="col">RIB</th>
+    </tr>
+  </thead> 
+   <tbody>
+  </tbody>
+  
+</table>
+<nav aria-label="Page navigation example" className="pg">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+        </section>
         </>
     )
 }
